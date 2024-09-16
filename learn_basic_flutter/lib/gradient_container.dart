dc9a1d181e735b717b2bafcb5ade2a9f1b941f71
SaktiPadmayoga <sakti14yoga@gmail.com>
2024-09-16 19:06:35 +0700

    applying dice roller method to container

--- learn_basic_flutter/lib/gradient_container.dart
+++ learn_basic_flutter/lib/gradient_container.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:learn_basic_flutter/dice_roller.dart';
 
-import 'package:learn_basic_flutter/styled_text.dart';
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          Color.fromARGB(255, 1, 255, 255),
-          Color.fromARGB(255, 255, 120, 1),
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-      )),
-      child: const Center(child: StyledText('anjayyy')),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
     );
   }
 }
