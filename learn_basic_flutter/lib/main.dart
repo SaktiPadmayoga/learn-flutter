3d56ced493dfef7b2027ef7084554da960e4cb30
SaktiPadmayoga <sakti14yoga@gmail.com>
2024-09-16 19:04:40 +0700

    adjust main code

--- learn_basic_flutter/lib/main.dart
+++ learn_basic_flutter/lib/main.dart
@@ -3,18 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:learn_basic_flutter/gradient_container.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  runApp(
+    const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(255, 68, 21, 149),
+        ),
       ),
-    );
-  }
+    ),
+  );
 }
